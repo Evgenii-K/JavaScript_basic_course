43ecfaacb2e7e69593becbb7bc3b166f1afdae18
Evgenii-K <72011038+Evgenii-K@users.noreply.github.com>
2021-01-28 20:40:08 +0700

    add script2

---
+++ Lesson_4/script2/mover.js
@@ -0,0 +1,64 @@
+'use strict';
+
+let mover = {
+    getDirection() {
+        const availableDirection = [1, 2, 3, 4, 6, 7, 8, 9];
+        while (true) {
+            let direction = +prompt('Введите число, куда вы хотите переместиться. "Отмена" для выхода', '');
+            if (isNaN(direction)) {
+                return null;
+            } else if (!availableDirection.includes(direction)) {
+                alert('Для перемещения необходимо ввести одно из чисел 1, 2, 3, 4, 6, 7, 8 или 9.');
+                continue;
+            } else {
+                return direction;
+            }
+        }
+    },
+
+    getNextPosition(direction) {
+        const nextPosition = {
+            x: player.x,
+            y: player.y
+        };
+
+        if ( (player.y === 0 && [7, 8, 9].includes(direction)) || 
+        ((player.y === (config.rowsCount - 1)) && [1, 2, 3].includes(direction)) ||
+        (player.x === 0 && [1, 4, 7].includes(direction)) ||
+        ((player.x === (config.colsCount - 1)) && [3, 6, 9].includes(direction)) ) {
+            return nextPosition;
+        } else {
+            switch (direction) {
+                case 1:
+                    nextPosition.x--;
+                    nextPosition.y++;
+                    break;
+                case 2:
+                    nextPosition.y++;
+                    break;
+                case 3:
+                    nextPosition.x++;
+                    nextPosition.y++;
+                    break;
+                case 4:
+                    nextPosition.x--;
+                    break;
+                case 6:
+                    nextPosition.x++;
+                    break;
+                case 7:
+                    nextPosition.x--;
+                    nextPosition.y--;
+                    break;
+                case 8:
+                    nextPosition.y--;
+                    break;
+                case 9:
+                    nextPosition.x++;
+                    nextPosition.y--;
+                    break;
+            }
+            return nextPosition;
+        }
+    }
+};
